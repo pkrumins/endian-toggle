06c6b99fa1631ef0a5187ebc8aa772e59aceaeda
James Halliday <mail@substack.net>
2012-10-31 18:59:48 -0700

    check for multiples of 8

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 module.exports = function (buf, bits) {
     var output = new Buffer(buf.length);
     
+    if (bits % 8 !== 0) {
+        throw new Error('bits must be a multiple of 8');
+    }
+    
     var bytes = bits / 8;
     if (buf.length % bytes !== 0) {
         throw new Error((buf.length % bytes) + ' non-aligned trailing bytes');
